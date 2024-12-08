84ac9336ae76f0a7e519a5d05fd5ef1441e46436
Arogunmasa Abayomi <nomorelimit11@gmail.com>
2024-12-08 21:38:10 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<header>
+			<nav>
+				<div className="logo__wrapper">
+					<img src="/assets/Chef Claude Icon.svg" alt="" />
+					<h3 className="logo">Chef Claude</h3>
+				</div>
+			</nav>
+		</header>
+	);
+};
+export default Header;
